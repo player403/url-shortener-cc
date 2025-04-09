7117100286281aa299aaec3a0958ff5893136928
Thomas Kronabeter <t.kronabeter@hotmail.com>
2025-04-09 10:10:57 +0200

    Add .env, rename .env variabes, and fix routing

--- index.js
+++ index.js
@@ -7,38 +7,48 @@ import env from 'dotenv';
 env.config();
 //TODO Expire checks
 var client = new localClient(process.env.SURL_URL_LENGTH);
-
-if(process.env.SURL_USE_POSTGRESQL === true){
+if(process.env.SURL_DATA_HANDLER === 'postgreSQL'){
     client = new ShortURLClient({
-        user: process.env.SURL_DB_USER, 
-        password: process.env.SURL_DB_PASSWORD, 
-        host: process.env.SURL_DB_HOST, 
-        port: process.env.SURL_DB_PORT, 
+        user: process.env.SURL_POSTGRESQL_USER, 
+        password: process.env.SURL_POSTGRESQL_PASSWORD, 
+        host: process.env.SURL_POSTGRESQL_HOST, 
+        port: process.env.SURL_POSTGRESQL_PORT, 
         database: 'url_shortener_db'});
-        await client.init();
-    //console.log(client.test);
+    await client.init();
 }
     
-
 const app = express();
 const port = 8080;
 
-
 app.use(express.urlencoded({'extended': true}));
 app.use(express.static('static'));
+
+
 
 app.get('/', (req, res) => {
     res.render('./index.ejs');
 });
 
 app.get('/view/:shortURL', async (req, res) => {
-    const entry = await client.getEntryByShortURL(req.params.shortURL);
-    res.render('view.ejs', {entry: entry});
+    try{
+        const entry = await client.getEntryByShortURL(req.params.shortURL);
+        if(entry.expireDate < new Date())
+            console.log("Expired");
+        res.render('view.ejs', {entry: entry});
+    }catch (e){
+        console.log(e.message);
+        res.sendStatus(404);
+    }
+    
 });
 
 app.get('/:shortURL', async (req, res) => {
     try{
         const entry = await client.getEntryByShortURL(req.params.shortURL);
+        if(!entry)
+            res.sendStatus(404);
+        if(entry.expireDate < new Date())
+            console.log("Expired");
         if (entry)
             res.redirect(entry.url);
         console.log(`Redirecting ${req.params.shortURL} to ${entry.url}`);
@@ -65,6 +75,7 @@ app.post('/del/:shortURL', async (req, res) => {
     await client.deleteEntryByShortURL(req.body.shortURL);
     res.sendStatus(200);
 });
+
 
 app.listen(port, (err) => {
     if (err)
