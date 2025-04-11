73577250a6d068874d7b49ec08928a2bf5e1d73a
Thomas <Thomas@KNOTEBOOK>
2025-04-11 14:44:28 +0200

    Rename database elements, fix database generator function

--- database.sql
+++ database.sql
@@ -1,38 +1,30 @@
-CREATE DATABASE "url_shortener_db"
-    WITH
-    ENCODING = 'UTF8';
-
-CREATE TABLE url_references(
-    short_url text PRIMARY KEY UNIQUE NOT NULL DEFAULT generate_short_url,
+CREATE TABLE urls(
+    id text PRIMARY KEY UNIQUE NOT NULL,
     url text UNIQUE NOT NULL,
     expire_date date
 );
 
 CREATE OR REPLACE FUNCTION
-generate_short_url(length INT DEFAULT 6)
+generate_alphanumerical(length INT DEFAULT 6)
 RETURNS TEXT AS $$
 DECLARE
 	chars TEXT := 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 	result TEXT := '';
+	exists_count INT;
 	i INT;
 	try_id TEXT;
 BEGIN 
-	--LOOP
+	LOOP
 		result := '';
 		FOR i IN 1..length LOOP
 			result := result || substr(chars, floor(random() * length(chars) + 1)::int, 1);
 		END LOOP;
-		--BEGIN
-			
-		--	IF (
-		--	SELECT count(short_url)
-		--	FROM url_references
-		--	WHERE short_url = result) > 0 AS bo THEN
-		--	CONTINUE; 
-		--	END IF;
-			
-		--END;
-		RETURN result;
-	--END LOOP;
+		SELECT COUNT(*) INTO exists_count FROM urls WHERE id = result;
+        IF exists_count = 0 THEN
+            RETURN result;
+        END IF;
+	END LOOP;
 END;
 $$ LANGUAGE plpgsql;
+
+ALTER TABLE urls ALTER COLUMN id SET DEFAULT generate_alphanumerical();
